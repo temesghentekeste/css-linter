0a63015caf543b87562a0148d3013194b10cc27a
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2020-07-13 01:06:51 +0300

    Add logic to get errors on opening curly brackets  - Refactored the code on Scanner class

--- lib/scanner.rb
+++ lib/scanner.rb
@@ -5,13 +5,31 @@ class Scanner
     @errors = []
     @lines = lines
     @space_scanner = SpaceScanner.new
-    scan_errors
+    fetch_errors
   end
 
-  def scan_errors
+  def fetch_errors
+    get_errors_on_last_line
+    get_errors_on_trailing_space
+    get_errors_on_opening_curly_bracket
+  end
+
+  def get_errors_on_last_line
     @errors << @space_scanner.last_line_scan(@lines)
+    
+  end
+
+  def get_errors_on_trailing_space
     @lines.each_with_index do |line, i|
       @errors << @space_scanner.trailing_space_scan(line, i)
+    end
+    
+  end
+
+  def get_errors_on_opening_curly_bracket
+    @lines.each_with_index do |line, i|
+      next unless line.include?('{')
+      @errors << @space_scanner.space_before_curly_bracket_scan(line, i)
     end
   end
 end
--- lib/space_scan.rb
+++ lib/space_scan.rb
@@ -1,12 +1,15 @@
 require 'byebug'
 class SpaceScanner
-
   def last_line_scan(lines)
     return 'trailing blank lines detected.' if lines[-1] == "\n" and lines[-2] == "\n"
     return 'final newline messing.' unless lines.last.include? "\n"
   end
 
   def trailing_space_scan(line, i)
-    "Trailing space found on line #{i + 1}." if line.end_with?("; \n")
+    "trailing space found on line #{i + 1}." if line.end_with?("; \n")
+  end
+
+  def space_before_curly_bracket_scan(line, i)
+    "there should be a space before curly bracket on line #{i + 1}" unless line.match(/[[:space:]]\{/) 
   end
 end
