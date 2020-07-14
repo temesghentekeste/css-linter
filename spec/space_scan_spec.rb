ede961cc2667b006da3f56bbb97a0d00888f91c0
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2020-07-14 18:45:54 +0300

    Add testing for trailing space scan

--- spec/space_scan_spec.rb
+++ spec/space_scan_spec.rb
@@ -4,9 +4,11 @@ require './lib/space_scan'
 
 describe SpaceScanner do
   subject { SpaceScanner.new() }
-  let(:lines_without_error) { [".primary-color:hover {\n", "  color: #1da1f2; \n", "}\n"]}
+  let(:lines_without_error) { [".primary-color:hover {\n", "  color: #1da1f2;\n", "}\n"]}
   let(:lines_with_end_line_error) { [".primary-color:hover {\n", "  color: #1da1f2; \n", "}\n\n"]}
   let(:last_line_missing) { [".primary-color:hover {\n", "  color: #1da1f2; \n", "}"]}
+  let(:lines_with_trailing_space) { [".primary-color:hover {\n", "  color: #1da1f2; \n", "}\n"]}
+  let(:errors) { []}
 
   describe 'last_line_scan' do
 
@@ -23,6 +25,25 @@ describe SpaceScanner do
       expect(subject.last_line_scan(last_line_missing)).to eql(":Line 4: \e[0;33;49mFormat error\e[0m: final newline messing.")
     end
 
+  end
+
+  describe 'trailing_space_scan' do
+
+    it 'should return nil if there is no trailing space' do
+      lines_without_error.each_with_index do |line, index|
+        errors << subject.trailing_space_scan(line, index)
+      end
+      errors.delete(nil)
+      expect(errors).to eql([])
+    end
+
+    it 'should return error message if there is a trailing space' do
+      lines_with_trailing_space.each_with_index do |line, index|
+        errors << subject.trailing_space_scan(line, index)
+      end
+      errors.delete(nil)
+      expect(errors.first.include?("trailing space found")).to eql(true)
+    end
   end
 end
 
